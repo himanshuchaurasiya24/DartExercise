b754f237e5f5803c7a184b0a1736960778f66232
himanshuchaurasiya24 <himanshuchaurasiya24@gmail.com>
2023-07-19 11:55:56 +0530

    variables

--- first.dart
+++ first.dart
@@ -1,6 +1,25 @@
 void main() {
-  print("Hello World!");
-  for (int i = 0; i < 11; i++) {
-    print("Hello World! " + i.toString());
-  }
+  String name = "Himanshu Chaurasiya!";
+  print(name);
+  bool firstValue = true;
+  print(firstValue);
+  dynamic dynamicValue = "Himanshu";
+  print(dynamicValue);
+  dynamic dynamicValue2 = 10;
+  print(dynamicValue2.runtimeType);
+  String greeting = "Hello Dart";
+  print(greeting);
+  greeting = "${greeting} Yoooo";
+  print(greeting);
+  greeting = "$greeting Yoooo";
+  print(greeting);
+  greeting = "${greeting.length} Yoooo";
+  print(greeting);
+  greeting = "$greeting ypmfekvjd";
+  print(greeting);
+  // for multi line strings we use
+  String multi = '''Hello
+  world
+  multiline string here''';
+  print(multi);
 }
